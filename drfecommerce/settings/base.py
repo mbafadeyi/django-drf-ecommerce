10308645fb9fb66e8b5915e876d79216e7f2b40c
M.B.A. Fadeyi <fadeyibajide@gmail.com>
2023-05-30 06:29:15 +0100

    Nc

--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
-from dotenv import load_dotenv
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -123,4 +124,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # REST_FRAMEWORK
-REST_FRAMEWORK={}
+REST_FRAMEWORK = {}
